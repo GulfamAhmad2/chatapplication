0b88d31dd07fe12961dff89f37e9b961d7888523
cb-here <cbv934@gmail.com>
2025-04-25 11:39:56 +0530

    basic setup in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Chat</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
